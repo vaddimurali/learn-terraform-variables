61814753dfaa4edec9deb021563d30a02213cd8e
murali <vaddimurali@gmail.com>
2025-07-30 13:24:14 -0700

    Change environment tag from dev to development

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.28.0"
+      version = "~> 3.22.0"
     }
   }
   required_version = ">= 0.14.0"
